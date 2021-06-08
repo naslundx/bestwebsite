f168b7f95d417721ecb40e4803b29b80a267e3fe
Marcus Näslund <naslundx@gmail.com>
2021-06-08 09:25:47 +0200

    Created my first files

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/secret', (req, res) => {
+    res.writeHead(200);
+    res.end("Hello");
+})
+
+app.use(express.static('public'))
+
+app.listen(8000);
