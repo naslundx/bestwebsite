4c3237758d3cda3dd5056bac4644ade936e1a552
Marcus Näslund <naslundx@gmail.com>
2021-06-09 09:53:07 +0200

    guestbook

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
-const app = express();
+const { query, validationResult } = require('express-validator');
 
-app.get('/secret', (req, res) => {
+const app = express();
+app.set('view engine', 'ejs');
+
+let messages = []
+
+app.get('/submit', [query("data").isString().isLength({min: 3, max: 50})], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.writeHead(400);
+        res.end(errors[0].msg);
+        return;
+    }
+
+    messages.push(req.query.data);
     res.writeHead(200);
-    res.end("Hello");
+    res.end("OK");
 })
+
+app.get("/", (req, res) => {
+    res.render("pages/index", {messages: messages.slice().reverse()});
+});
 
 app.use(express.static('public'))
 
